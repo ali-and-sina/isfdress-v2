edafaf083a32ea47e0b55c0c3a97c9a1b12a1efb
AlirezaAlikhani <alireza.alikhani10@gmail.com>
2026-07-26 21:26:40 +0330

    creating profile page

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,11 @@ const nextConfig = {
         hostname: "i.imgur.com",
         port: "",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+      },
     ],
   },
 };
