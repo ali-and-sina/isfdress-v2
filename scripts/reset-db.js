aee39b5c17b815b4b20e2d963dfdc6da6f293697
sina <cna.ajj@gmail.com>
2026-09-03 17:27:03 +0330

    made other db as well

--- scripts/reset-db.js
+++ scripts/reset-db.js
@@ -3,10 +3,13 @@ import { query } from "../src/lib/db.js";
 // Order matters less here since CASCADE handles dependents,
 // but listed in reverse-FK order for clarity.
 const dropSql = `
+  DROP TABLE IF EXISTS order_items CASCADE;
+  DROP TABLE IF EXISTS orders CASCADE;
   DROP TABLE IF EXISTS product_variants CASCADE;
   DROP TABLE IF EXISTS product_images CASCADE;
   DROP TABLE IF EXISTS products CASCADE;
   DROP TABLE IF EXISTS categories CASCADE;
+  DROP TABLE IF EXISTS users CASCADE;
 `;
 
 await query(dropSql);
