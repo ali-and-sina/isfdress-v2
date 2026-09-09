fe0c825ec7c836cb301646e2c515993dcb2e5b2f
Alireza <alireza.alikhani10@gmail.com>
2026-09-09 17:46:56 +0330

    Delete scripts directory

--- scripts/db-init.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs/promises";
-import { query } from "../src/lib/db.js";
-
-const schema = await fs.readFile("./database/schema.sql", "utf8");
-
-await query(schema);
-
-console.log("✅ Schema created.");
-process.exit(0);
